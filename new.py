f6b788b754b8b18da16d6ea74bf8be19c17ec7bf
yashar alizadeh <yasharian1383@gmail.com>
2025-03-10 02:30:01 +0330

    2 mamamia

--- new.py
+++ new.py
@@ -1,10 +1,11 @@
+import torch , numpy 
 def main() : 
 	x = torch.tensor([[ i*0.4 for i in range(12)] for j in range(3) ] ) 
 	tt = x.numpy() 
 	tt.t_() 
 	print(x) 
 	print(tt)  
-
+	torch.cuda.is_available() 
 
 if __name__ == "__main__" : 
 	print("hello my men ") 
